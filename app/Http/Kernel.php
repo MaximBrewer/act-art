a81c2667e2d7a28567b3c16bf3363703193359ea
MaxBrewer <pimax1978@icloud.com>
2020-07-31 16:22:08 +0500

    hkjh

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,8 +44,8 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
             \App\Http\Middleware\Locale::class,
         ],
     ];
