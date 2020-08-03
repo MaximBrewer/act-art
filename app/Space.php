4c39021f6e1d679188c4b919d8a8dfcbcd3b77b2
MaxBrewer <pimax1978@icloud.com>
2020-08-03 18:41:27 +0500

    dfd

--- app/Space.php
+++ app/Space.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use TCG\Voyager\Traits\Spatial;
+use App\Traits\Voyager\Spatial;
 use TCG\Voyager\Traits\Translatable;
 
 class Space extends Model
